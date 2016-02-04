fee2045c0ee20ac0e32310af2123a5d3c116b961
Alex Gontar <mosegontar@gmail.com>
2016-02-04 18:48:39 -0500

    Add peewee to requirements.txt and fix setup.py typos

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup
 
 setup(
-    name = 'scratchtrack'
-    version = '1.0'
-    author = 'Alex Gontar'
-    url = 'https://github.com/mosegontar/ScratchTrack.git'
-    description = 'Tag and track miscellaneous files in local directories'
-    license = 'MIT'
-    packages = ['scratchtrack']
-    install_requires = []
+    name = 'scratchtrack',
+    version = '1.0',
+    author = 'Alex Gontar',
+    url = 'https://github.com/mosegontar/ScratchTrack.git',
+    description = 'Tag and track miscellaneous files in local directories',
+    license = 'MIT',
+    packages = ['scratchtrack'],
+    install_requires = [],
     entry_points = {
-        'console_scripts': ['strack = scratchtrack.main:run]
+        'console_scripts': ['strack = scratchtrack.main:run']
     }
 
 )
